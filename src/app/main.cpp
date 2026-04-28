b6de245cbf57abc966b0ef445832360e5cd3902a
meister212 <nik.dmitriev06@mail.ru>
2026-04-28 09:58:28 +0300

    added thread

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,8 +7,14 @@ int main() {
 
     TaskManager manager(4);
 
+    manager.CreateTask("sleep", "1000");
     manager.CreateTask("print", "Hello\n");
     manager.CreateTask("sleep", "1000");
 
+    std::cout << "enter any symbol to start";
+    std::cin.get();
+    manager.start();
     std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    std::cin.get();
 }
